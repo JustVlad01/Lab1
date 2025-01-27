106b64ffac72bf0ec8669c2c6d77d1c51e880c33
JustVlad01 <s00233992@atu.ie>
2025-01-27 16:33:04 +0000

    Add project files.

---
+++ Week1Lab12025/Program.cs
@@ -0,0 +1,40 @@
+using Tracker.WebAPIClient;
+
+namespace Week1Lab12025
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+            ActivityAPIClient.Track(StudentID: "S00233992", StudentName: "Vlad khokha", activityName: "Rad302 2025 Week 1 Lab 1", Task: "Project Setup");
+            app.Run();
+        }
+    }
+}
